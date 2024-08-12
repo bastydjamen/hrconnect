893a6c0c45f4497dd25781b04edb99a91d42dcec
eugenie Bastiane Djamen <bastiane@MacBook-Pro-von-eugenie.local>
2024-08-12 17:00:00 +0200

    #7 creation des JPA Repositories

---
+++ backend/src/main/java/fr/afrogeek/geekhrconnect/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package fr.afrogeek.geekhrconnect.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.afrogeek.geekhrconnect.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee , UUID>{
+
+}
+ 
